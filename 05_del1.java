ec91b1a78381bcc7a0951e9f14f6c472d2ad435f
Soeren_Kjaer <soeren.kjaer89@gmail.com>
2023-09-19 15:39:14 +0200

    main prototype of 05_del1 completed

--- 05_del1.java
+++ 05_del1.java
@@ -2,12 +2,28 @@ class 05_del1 {
 
     public static void main(String[] args) {
 
+    var s = new java.util.scanner(System.in);
+
     var die1 = new Die();
     var die2 = new Die();
 
-    die1.getValue();
-    die2.getValue();
-    
+    while (s.hasNextLine()) {
+        var roll = s.nextLine();
+        switch(roll) { 
+            case "roll" -> {
+                var value1 = die1.getValue();
+                var value2 = die2.getValue();
+                var sum = value1 + value2;
+                System.out.println("Value of die 1 is " + value1 + " Value of die 2 is " + value2 + " The sum of both dice is " + sum);
+            }
+            default -> {
+                System.out.println("If you wanna make another roll, type 'roll'");
+            }
+        } 
+    }
+
+    s.close();
+
     }
 }
 
