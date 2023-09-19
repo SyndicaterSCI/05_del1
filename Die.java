dcce7350a282cbc62a26fb88ede46561b1d145a1
Marcus Lemser <mle91@protonmail.com>
2023-09-19 14:51:40 +0200

    Oprettet Die class

--- Die.java
+++ Die.java
@@ -1,8 +1,21 @@
-class 05_del1 {
+import java.util.Random;
 
-    public static void main(String[] args) {
+class Die {
+    int faceValue;
 
+    // Generate a random number between 1 and 6, and sets the variable "faceValue" to that number. 
+    // It simulates a dice roll.
+    public void roll(){
+        int min = 1;
+        int max = 6;
+        Random rand = new Random();
+        faceValue = rand.nextInt((max - min) + 1) + min;
+    }
 
+    // "Rolls" the die and returns the number of the face value of the die, as an integer.
+    public int getValue(){
+        roll();
+        return faceValue;
     }
 }
 
