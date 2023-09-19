f89738cba5a303f1d123fd4b146c50b346fb5feb
Soeren_Kjaer <soeren.kjaer89@gmail.com>
2023-09-19 14:55:26 +0200

    creation of initial die1+2 var

--- 05_del1.java
+++ 05_del1.java
@@ -2,7 +2,12 @@ class 05_del1 {
 
     public static void main(String[] args) {
 
+    var die1 = new Die();
+    var die2 = new Die();
 
+    die1.getValue();
+    die2.getValue();
+    
     }
 }
 
