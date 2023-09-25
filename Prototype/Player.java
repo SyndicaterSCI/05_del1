9c532896f07370aac745dc477bccaf09b40d2308
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-09-25 16:04:13 +0200

    Fikset et bug og lavet kommentarer

--- Prototype/Player.java
+++ Prototype/Player.java
@@ -1,25 +1,28 @@
 public class Player {
 
     // Should make a integer that is "Totalscore"
-public int = totalscore;
+    public int totalScore = 0;
 
+    // Generate a string for a player name
 
-//Should generate a string that makes the user input a name
-Player(String name){
-    this.name = name;
-}
+    public String name;
 
-    String getName() {
+    public Player(String name) {
+        this.name = name;
+    }
+
+    // Gets the name from the current player and returns it
+    public String getName() {
         return name;
     }
 
     // Should get the totalscore from the "Game" and then display it to the player
-    int getTotalScore() {
+    public int getTotalScore() {
         return totalScore;
     }
 
     // Calculate the score, not return value
-    void setTotalScore(int score) {
+    public void setTotalScore(int score) {
         totalScore += score;
     }
 }
