3ac9c2ffa529b176e34d288e622ee1ea5420ba39
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-09-25 15:32:54 +0200

    Player First Draft

---
+++ Prototype/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+
+    // Should make a integer that is "Totalscore"
+public int = totalscore;
+
+
+//Should generate a string that makes the user input a name
+Player(String name){
+    this.name = name;
+}
+
+    String getName() {
+        return name;
+    }
+
+    // Should get the totalscore from the "Game" and then display it to the player
+    int getTotalScore() {
+        return totalScore;
+    }
+
+    // Calculate the score, not return value
+    void setTotalScore(int score) {
+        totalScore += score;
+    }
+}
