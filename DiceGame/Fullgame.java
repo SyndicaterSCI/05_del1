88831c865ae982405bd8fe18c4112e006c1e9ee2
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-09-26 13:04:28 +0200

    Fikset Double Roll tekst

--- DiceGame/Fullgame.java
+++ DiceGame/Fullgame.java
@@ -18,7 +18,8 @@ class Fullgame {
         System.out.println("Welcome to the table " + player1.getName() + "\n Enter the name of Player 2");
         player2 = new Player(input.nextLine());
         System.out.println("Welcome to the table " + player2.getName());
-        System.out.println("Type 'Roll' to Roll THEM DICE! " + "Its " + player1.getName() + " turn");
+        // System.out.println("Type 'Roll' to Roll THEM DICE! " + "Its " +
+        // player1.getName() + " turn");
 
         for (int i = 1; flag; i++) {
             if ((i % 2) != 0) {
@@ -27,79 +28,84 @@ class Fullgame {
                 play(player2);
             }
         }
-        
+
     }
 
-/*
- magi call static til
- boolean (flag = true)
- die 
- player
- */
+    /*
+     * magi call static til
+     * boolean (flag = true)
+     * die
+     * player
+     */
 
-/*
-Scanner for game rule
+    /*
+     * Scanner for game rule
+     * 
+     * 
+     * for loop med flag "skift mellem player 1 og player 2"
+     * 
+     * 
+     * 
+     * 
+     * gameplay loop
+     */
+    static void play(Player player) {
+        System.out.println("\nTurn of: " + player.getName()
+                + "\nEnter one of the following commands: 'Roll'To ROLL THEM DICE or 'Show points' or 'Exit'");
+        boolean playFlag = true;
+        while (playFlag) {
+            Scanner input = new Scanner(System.in);
+            var playCommand = input.nextLine();
+            switch (playCommand.toLowerCase()) {
+                case "roll" -> {
+                    die1.roll();
+                    die2.roll();
+                    int sum = die1.getValue() + die2.getValue();
+                    player.setTotalScore(sum);
 
-
-for loop med flag "skift mellem player 1 og player 2"
-
-
-
-
-gameplay loop
- */
-static void play(Player player){
-    System.out.println("\nTurn of: " + player.getName() + "\nEnter one of the following commands: 'Roll' or 'Show points' or 'Exit'");
-    boolean playFlag = true;
-    while(playFlag){
-        Scanner input = new Scanner(System.in);
-        var playCommand = input.nextLine();
-        switch(playCommand.toLowerCase()){
-            case "roll" -> {
-                die1.roll();
-                die2.roll();
-                int sum = die1.getValue() + die2.getValue();
-                player.setTotalScore(sum);
-
-                System.out.println("\nValue of the first die is " + die1.getValue() + "\nValue of the second die is " + die2.getValue());
-                System.out.println("Total points of " + player.getName() + ": " + player.getTotalScore());
-                verifyWin(player);
-                playFlag = false;
-            }
-            case "show points" -> {
-                System.out.println("Total Points of " + player1.getName() + ": " + player1.getTotalScore() + "\nTotal Points of " + player2.getName() + ": " + player2.getTotalScore());
-            }
-            case "exit" -> {
-                System.out.println("Thank you for playing!");
-                playFlag = false;
-                flag = false;
-            }
-            default -> {
-                System.out.println("\nCommand not understood. Try again");
+                    System.out.println("\nValue of the first die is " + die1.getValue()
+                            + "\nValue of the second die is " + die2.getValue());
+                    System.out.println("Total points of " + player.getName() + ": " + player.getTotalScore());
+                    verifyWin(player);
+                    playFlag = false;
+                }
+                case "show points" -> {
+                    System.out.println("Total Points of " + player1.getName() + ": " + player1.getTotalScore()
+                            + "\nTotal Points of " + player2.getName() + ": " + player2.getTotalScore());
+                }
+                case "exit" -> {
+                    System.out.println("Thank you for playing!");
+                    playFlag = false;
+                    flag = false;
+                }
+                default -> {
+                    System.out.println("\nCommand not understood. Try again");
+                }
             }
         }
     }
-}
 
-// features 
+    // features
 
-/*
-verifyWin 
+    /*
+     * verifyWin
+     * 
+     * setTotalScore(sum)
+     */
 
-setTotalScore(sum)*/
-
-
- static void verifyWin(Player player){
-    if(player.getTotalScore() >= 40){
-        System.out.println("Congratulations! " + player.getName() + " has won the game with " + player.getTotalScore() + " points");
-        flag = false;
+    static void verifyWin(Player player) {
+        if (player.getTotalScore() >= 40) {
+            System.out.println("Congratulations! " + player.getName() + " has won the game with "
+                    + player.getTotalScore() + " points");
+            flag = false;
+        }
     }
-}
 
-static void standardPro(int sum, Player player){
-    player.setTotalScore(sum);
-    System.out.println("\nValue of first die is " + die1.getValue() + "\nValue of second die is " + die2.getValue());
-    System.out.println("Total point of " + player.getName() + ": " + player.getTotalScore());
+    static void standardPro(int sum, Player player) {
+        player.setTotalScore(sum);
+        System.out
+                .println("\nValue of first die is " + die1.getValue() + "\nValue of second die is " + die2.getValue());
+        System.out.println("Total point of " + player.getName() + ": " + player.getTotalScore());
     }
 
 }
