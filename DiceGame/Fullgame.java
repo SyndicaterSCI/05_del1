762efab90f088dfb4a48f004e0da9365ff452b69
Hjalte <hjaltemb2@gmail.com>
2023-09-26 11:37:36 +0200

    Fixed some small coding issues

--- DiceGame/Fullgame.java
+++ DiceGame/Fullgame.java
@@ -15,10 +15,10 @@ class Fullgame {
         System.out.println("Enter the name of Player 1");
         player1 = new Player(input.nextLine());
 
-        System.out.println("Welcome to the table " + player1.name + "\n Enter the name of Player 2");
+        System.out.println("Welcome to the table " + player1.getName() + "\n Enter the name of Player 2");
         player2 = new Player(input.nextLine());
-        System.out.println("Welcome to the table " + player2.name);
-        System.out.println("Type 'Roll' to Roll THEM DICE! " + "Its " + player1.name + " turn");
+        System.out.println("Welcome to the table " + player2.getName());
+        System.out.println("Type 'Roll' to Roll THEM DICE! " + "Its " + player1.getName() + " turn");
 
         for (int i = 1; flag; i++) {
             if ((i % 2) != 0) {
@@ -49,7 +49,7 @@ for loop med flag "skift mellem player 1 og player 2"
 gameplay loop
  */
 static void play(Player player){
-    System.out.println("\nTurn of: " + player.name + "\nEnter one of the following commands: 'Roll' or 'Show points' or 'Exit'");
+    System.out.println("\nTurn of: " + player.getName() + "\nEnter one of the following commands: 'Roll' or 'Show points' or 'Exit'");
     boolean playFlag = true;
     while(playFlag){
         Scanner input = new Scanner(System.in);
@@ -58,16 +58,16 @@ static void play(Player player){
             case "roll" -> {
                 die1.roll();
                 die2.roll();
-                int sum = die1.faceValue + die2.faceValue;
+                int sum = die1.getValue() + die2.getValue();
                 player.addPoint(sum);
 
-                System.out.println("\nValue of the first die is " + die1.faceValue + "\nValue of the second die is " + die2.faceValue);
-                System.out.println("Total points of " + player.name + ": " + player.point);
+                System.out.println("\nValue of the first die is " + die1.getValue() + "\nValue of the second die is " + die2.getValue());
+                System.out.println("Total points of " + player.getName() + ": " + player.getTotalScore());
                 verifyWin(player);
                 playFlag = false;
             }
             case "show points" -> {
-                System.out.println("Total Points of " + player1.name + ": " + player1.point + "\nTotal Points of " + player2.name + ": " + player2.point);
+                System.out.println("Total Points of " + player1.getName() + ": " + player1.getTotalScore() + "\nTotal Points of " + player2.getName() + ": " + player2.getTotalScore());
             }
             case "exit" -> {
                 System.out.println("Thank you for playing!");
@@ -94,15 +94,15 @@ addPoint
 
  static void verifyWin(Player player){
     if(player.point >= 40){
-        System.out.println("Congratulations! " + player.name + " has won the game with " + player.point + " points");
+        System.out.println("Congratulations! " + player.getName() + " has won the game with " + player.getTotalScore() + " points");
         flag = false;
     }
 }
 
 static void standardPro(int sum, Player player){
     player.addPoint (sum);
-    System.out.println("\nValue of first die is " + die1.faceValue + "\nValue of second die is " + die2.faceValue);
-    System.out.println("Total point of " + player.name + ": " + player.point);
+    System.out.println("\nValue of first die is " + die1.getValue() + "\nValue of second die is " + die2.getValue());
+    System.out.println("Total point of " + player.getName() + ": " + player.getTotalScore());
     }
 
 }
