d4583d9a14b9304191d1bb67b09aaf9b6e83915c
Gustav.S.H.M <s235783@dtu.dk>
2023-09-26 08:39:50 +0200

    fullgame fil with base code placing

---
+++ Prototype/Fullgame.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Fullgame {
+/*
+ magi call static til
+ boolean (flag = true)
+ die 
+ player
+ */
+
+/*
+Scanner for game rule
+
+
+for loop med flag "skift mellem player 1 og player 2"
+
+
+
+
+gameplay loop
+ */
+
+
+// features 
+
+/*
+verifyWin 
+
+addPoint 
+
+
+ */
+
+
+
+
+}
