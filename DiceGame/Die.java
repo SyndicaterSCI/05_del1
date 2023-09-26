85535af3c4ab4aa9a9a9834f91548ef0c64cebcd
bearwithbeer <144028286+bearwithbeer@users.noreply.github.com>
2023-09-26 09:55:21 +0200

    VI prøver at rytte op i GIT

---
+++ DiceGame/Die.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+class Die {
+    public int faceValue;
+
+    // Generate a random number between 1 and 6, and sets the variable "faceValue"
+    // to that number.
+    // It simulates a dice roll.
+    public void roll() {
+        int min = 1;
+        int max = 6;
+        Random rand = new Random();
+        faceValue = rand.nextInt((max - min) + 1) + min;
+    }
+
+    // "Rolls" the die and returns the number of the face value of the die, as an
+    // integer.
+    public int getValue() {
+        return faceValue;
+    }
+}
---
+++ DiceGame/Player.java
@@ -0,0 +1,28 @@
+public class Player {
+
+    // Should make a integer that is "Totalscore"
+    public int totalScore = 0;
+
+    // Generate a string for a player name
+
+    public String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    // Gets the name from the current player and returns it
+    public String getName() {
+        return name;
+    }
+
+    // Should get the totalscore from the "Game" and then display it to the player
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    // Calculate the score, not return value
+    public void setTotalScore(int score) {
+        totalScore += score;
+    }
+}
