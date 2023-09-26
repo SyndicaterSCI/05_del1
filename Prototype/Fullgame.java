8ed4e9491b506111bbbeb31d887c2a22d3ad8203
Gustav.S.H.M <s235783@dtu.dk>
2023-09-26 09:09:59 +0200

    addPoint og verifyWin skrevet ind

--- Prototype/Fullgame.java
+++ Prototype/Fullgame.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 class Fullgame {
+
+
 /*
  magi call static til
  boolean (flag = true)
@@ -32,6 +34,17 @@ addPoint
  */
 
 
+ static void verifyWin(Player player){
+    if(player.point >= 40){
+        System.out.println("Congratulations! " + player.name + " has won the game with " + player.point + " points");
+        flag = false;
+    }
+}
 
+static void standardPro(int sum, Player player){
+    player.addPoint (sum);
+    System.out.println("\nValue of first die is " + die1.faceValue + "\nValue of second die is " + die2.faceValue);
+    System.out.println("Total point of " + player.name + ": " + player.point);
+    }
 
 }
